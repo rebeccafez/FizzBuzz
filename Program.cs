e6c34f8f444773f58863f255a393edc180a15bf7
Rebecca Fessenden <rebecca@rebeccafessenden.com>
2024-08-31 14:21:44 -0400

    Initial and only

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int current = 0;
+int amount = 0;
+while (amount <= 100)
+{
+    bool fizz = current % 3 == 0;
+    bool buzz = current % 5 == 0;
+    if (fizz && buzz)
+    {
+        Console.WriteLine(current);
+        amount++;
+    }
+    current++;
+}
